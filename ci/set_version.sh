0ee94ba132b512d1f69f66e5de55e175ee98761d
rtroe-xps <rob.tom.roe@gmail.com>
2023-03-20 07:35:13 -0700

    Updated Doxygen routes

--- ci/set_version.sh
+++ ci/set_version.sh
@@ -3,7 +3,7 @@ echo "Set Assembly Version"
 VERSION=2.0.0
 PROJECT_DIR="./src/shared"
 CSPROJECT_DESKTOPGL="../desktop.gl/VerticesEngine.Core.Desktop.GL.csproj"
-#DOX_PATH="./docs/Doxyfile"
+DOX_PATH="./docs/Doxyfile"
 
 
 echo "Setting Assembly and Nuget Pacakge versions to: "$VERSION.$@
@@ -15,7 +15,7 @@ sed -E -i "s/0.1.0/$VERSION.$@-alpha/g" "$PROJECT_DIR/$CSPROJECT_DESKTOPGL"
 #sed -E -i "s/0.1.0/$VERSION.$@-alpha/g" "$PROJECT_DIR.Task/${CSPROJECT_TASK}"
 
 # Set Doc Version
-#sed -E -i "s/v1.0.0/v$VERSION.$@/g" ${DOX_PATH}
+sed -E -i "s/v1.0.0/v$VERSION.$@/g" ${DOX_PATH}
 
 # Set Assembly Version
 sed -E -i "s/2.0.0.999/$VERSION.$@/g" "$PROJECT_DIR/Properties/AssemblyInfo.cs"
